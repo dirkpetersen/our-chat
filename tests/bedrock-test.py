f68e497325c7fbd483bb2fbabb9899910a049eea
Dirk Petersen <dipeit@gmail.com>
2024-10-04 12:25:37 -0700

    config update

--- tests/bedrock-test.py
+++ tests/bedrock-test.py
@@ -1,25 +1,24 @@
 #! /usr/bin/env python3
 
-import os, boto3, json
+import json
+import boto3
 
-#reg = os.getenv('AWS_REGION_NAME','us-west-2')
-#br = boto3.client(service_name="bedrock", region_name=reg)
 br = boto3.client(service_name="bedrock")
-
-# Let's see all available Anthropic Models
 available_models = br.list_foundation_models()
-
+print('\nList of available models on AWS Bedrock:\n')
 amodel = None
 for model in available_models['modelSummaries']:
     if 'anthropic' in model['modelId']:
         amodel = model['modelId']
     print(model['modelId'])
 
+
 bedrock = boto3.client(service_name="bedrock-runtime")
 body = json.dumps({
   "max_tokens": 256,
   "messages": [{"role": "user", "content": "Hello, world"}],
+  "anthropic_version": "bedrock-2023-05-31",
 })
 response = bedrock.invoke_model(body=body, modelId=amodel)
 response_body = json.loads(response.get("body").read())
-print("Response to 'Hello, world':",response_body.get("content"), flush=True)
+print("\nResponse to 'Hello, world':",response_body.get("content")[0]['text'], flush=True)
